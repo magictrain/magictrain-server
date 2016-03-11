b92a1c13cce1b5b4ff445673db1aed4d93ba7474
najiji <thomas.debrunner@hotmail.com>
2016-03-12 00:45:02 +0100

    initial commit

--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var Cloudant = require('cloudant')
+var bodyParser = require('body-parser');
+var cloudant = Cloudant('https://84263529-892c-4d7b-814c-cfcd113aa1f8-bluemix:860afff06af3febc99c4ea45b5f34abcbff2f87835a77aeb63dcc32fa5c6c871@84263529-892c-4d7b-814c-cfcd113aa1f8-bluemix.cloudant.com');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -14,6 +17,7 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+app.use(bodyParser.json());
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -27,3 +31,9 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+
+app.post('/push', function(req, res) {
+	console.log(req.body)
+	res.send(req.body)
+})
